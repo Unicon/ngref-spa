c2930bb36bf53e89775f8f421a58295bd5650af0
pneff <pneff@unicon.net>
2018-03-08 19:37:08 -0700

    multiple logins

--- src/main/resources/static/js/loginHelper.js
+++ src/main/resources/static/js/loginHelper.js
@@ -17,4 +17,20 @@ function buildLoginLink() {
 
 }
 
+function buildAuth0LoginLink() {
+    var auth0IdentityServer = "https://parkerneff.auth0.com/authorize";
+    var auth0Scope = "read:alljokes%20contacts";
+    var auth0Audience = "http://chucknorrisworld.com";
+    var auth0ResponseType = "id_token%20token";
+    var auth0ClientId = "jn0fyjN7sL0hU2nK27Rqy4Fk0JyTnl3D";
+
+    return auth0IdentityServer + "?scope=" + auth0Scope
+        + "&audience=" + auth0Audience
+        + "&response_type=" + auth0ResponseType
+        + "&client_id=" + auth0ClientId
+        + "&redirect_uri=" + redirectUri
+        + "&nonce=" + nonce;
+
+}
+
 
